97a08d456d0e753327fa232fdef689175a28fd05
Wojtek Malarski <w.malarski@gmail.com>
2025-11-08 22:49:51 +0100

    build still works

--- apps/astro/astro.config.mjs
+++ apps/astro/astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
+
+import node from "@astrojs/node";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  adapter: node({ mode: "standalone" }),
   build: {
     assets: "_fragment/astro/assets",
   },
