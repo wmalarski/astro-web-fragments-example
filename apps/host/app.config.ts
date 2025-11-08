67713b55c61ea0b36c7194129546bf11c24dc27c
Wojtek Malarski <w.malarski@gmail.com>
2025-11-08 10:58:42 +0100

    adds cloudflare to host

--- apps/host/app.config.ts
+++ apps/host/app.config.ts
@@ -1,3 +1,8 @@
 import { defineConfig } from "@solidjs/start/config";
 
-export default defineConfig({});
+export default defineConfig({
+  server: {
+    compatibilityDate: "2025-11-07",
+    preset: "cloudflare_module",
+  },
+});
