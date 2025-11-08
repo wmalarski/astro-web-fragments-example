bcc92d20dab6813a94cdb122f119e304648cfaf0
Wojtek Malarski <w.malarski@gmail.com>
2025-11-08 23:46:36 +0100

    adds integration with tmdb

--- apps/astro/astro.config.mjs
+++ apps/astro/astro.config.mjs
@@ -2,12 +2,17 @@
 
 import node from "@astrojs/node";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "astro/config";
+import { defineConfig, envField } from "astro/config";
 
 export default defineConfig({
   adapter: node({ mode: "standalone" }),
   build: {
     assets: "_fragment/astro/assets",
+  },
+  env: {
+    schema: {
+      TMDB_API_KEY: envField.string({ access: "public", context: "server" }),
+    },
   },
   vite: {
     plugins: [tailwindcss()],
