46cc281009e01d0ce827fedfdb7d418c2666d32b
Wojtek Malarski <w.malarski@gmail.com>
2025-11-08 19:46:39 +0100

    removes solid start app

--- packages/ui/src/code.tsx
+++
@@ -1,9 +0,0 @@
-import type { Component, ParentProps } from "solid-js";
-
-type CodeProps = ParentProps<{
-  class?: string;
-}>;
-
-export const Code: Component<CodeProps> = (props) => {
-  return <code class={props.class}>{props.children}</code>;
-};
