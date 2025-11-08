6278bd3251f63ca5b246b12ec8c85d1dda88d8e1
Wojtek Malarski <w.malarski@gmail.com>
2025-11-08 20:08:56 +0100

    working tailwindcss added

--- apps/astro/astro.config.mjs
+++ apps/astro/astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   build: {
     assets: "_fragment/homepage/assets",
-    // assetsPrefix: "_fragment/homepage/assets",    
+    // assetsPrefix: "_fragment/homepage/assets",
+  },
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div>
       <main>
+        <span className="font-bold text-3xl underline">Hello</span>
         <WebFragment fragmentId="homepage-2" src="/" />
       </main>
     </div>
